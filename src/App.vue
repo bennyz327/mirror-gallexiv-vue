568f88918f0c7032df5f310c1d6021ddabc79dba
benny3273 <benny@gmail.com>
2023-08-17 03:50:15 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup>
+import PostManager from "./PostManager.vue";
+</script>
+
+<template>
+  <div id="banner">
+    <h1>貼文(標籤)管理頁面</h1>
+  </div>
+  <PostManager/>
+</template>
---
+++ src/PostManager.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="container">管理介面內容</div>
+</template>
+<script>
+export default {
+  name: 'PostManager'
+}
+</script>

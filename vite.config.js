fdc0f52dccf78c10a2e2df3c4a4044b585336219
benny3273 <benny@gmail.com>
2023-08-31 01:33:26 +0800

    修正導入路徑和配置vite.config

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,42 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path from 'path'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-  //設定port
-    server: {
-        port: 3100,
-    }
-})
+// 官方文檔 https://vitejs.dev/config/
+export default defineConfig(
+    {
+        server: {
+            //端口
+            port: 3100,
+            //host設為true監聽所有地址
+            host: '0.0.0.0',
+            //端口被占用直接退出
+            strictPort: true,
+            //是否開啟https
+            https: false,
+            //跨域設定
+            cors: true,
+            /*
+            //回傳如下結構可以設定cors的規則
+            // {
+            //     "origin": "*",
+            //     "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+            //     "preflightContinue": false,
+            //     "optionsSuccessStatus": 204
+            // }
+            */
+        },
+        plugins: [vue()],
+        resolve: {
+            // 在導入語句沒帶文件後綴時，webpack會自動按照順序添加後綴名查找文件是否存在
+            extensions: [
+                '.js',
+                '.vue',
+                '.json',
+            ],
+            alias: {
+                '@': path.resolve(__dirname, './src'),
+            },
+        },
+
+    })

99040397e4d5e2cccc555364fcf53feac3f4286a
vic0716 <vic3962449@gmail.com>
2023-09-11 10:42:06 +0800

    圖片，標籤，作者完成

--- src/components/PostPictureView.vue
+++ src/components/PostPictureView.vue
@@ -4,29 +4,28 @@ import {computed, onBeforeMount, onBeforeUpdate, onMounted, onUpdated, reactive,
 
 // 傳回物件
 const props = defineProps({
-    imgUrlList : Object
+  imgUrlList: Object
 })
 
 
-  const liked = ref([]);
-  const hovered = ref([]);
+const liked = ref([]);
+const hovered = ref([]);
 
-  const toggleLike = (index) => {
-    liked.value[index] = !liked.value[index];
+const toggleLike = (index) => {
+  liked.value[index] = !liked.value[index];
+};
+
+const heartClass = computed(() => {
+  return (index) => {
+    if (liked.value[index] && hovered.value[index]) {
+      return 'fa-solid fa-heart fa-bounce fa-lg';
+    } else if (!liked.value[index] && hovered.value[index]) {
+      return 'fa-regular fa-heart fa-bounce fa-lg';
+    } else {
+      return liked.value[index] ? 'fa-solid fa-heart fa-lg' : 'fa-regular fa-heart fa-lg';
+    }
   };
-
-  const heartClass = computed(() => {
-    return (index) => {
-      if (liked.value[index] && hovered.value[index]) {
-        return 'fa-solid fa-heart fa-bounce fa-lg';
-      } else if (!liked.value[index] && hovered.value[index]) {
-        return 'fa-regular fa-heart fa-bounce fa-lg';
-      } else {
-        return liked.value[index] ? 'fa-solid fa-heart fa-lg' : 'fa-regular fa-heart fa-lg';
-      }
-    };
-  });
-
+});
 
 </script>
 
@@ -37,7 +36,7 @@ const props = defineProps({
     <div class="galley-middle-block">
       <div class="picture-galley-block">
         <div class="picture-item-div" v-for="item in imgUrlList.value">
-          <a target="_blank" :href="'/posts/' + item.postId">
+          <a target="_blank" :href="'/posts/' + item.userinfoByUserId.postId">
             <img v-if="item.blobUrl" :src="item.blobUrl" alt="pic"
                  style="width: 240px; height: 240px; object-fit: cover; border-radius: 8px;"
                  class="picture-div">
@@ -52,26 +51,25 @@ const props = defineProps({
 
             <div class="picture-item-user-div">
               <div class="picture-item-user-icon-div">
-                <router-link :to="'/user/' + item.userId">
-                  <img :src="item.userImg" alt="User" width="32" height="32" class="rounded-circle"
+                <router-link :to="'/user/' + item.userinfoByUserId.userId">
+                  <img :src="item.userinfoByUserId.avatar" alt="User" width="32" height="32" class="rounded-circle"
                        style="object-fit: cover;border: 1px solid #ccc;"/>
                 </router-link>
               </div>
 
               <div class="picture-item-user-name-div">
-                <router-link :to="'/user/' + item.userId" style="text-decoration:none; color:inherit; float: left">
-                  <p>{{ item.userName }}</p>
+                <router-link :to="'/user/' + item.userinfoByUserId.userId"
+                             style="text-decoration:none; color:inherit; float: left">
+                  <p>{{ item.userinfoByUserId.userName }}</p>
                 </router-link>
               </div>
 
-                            <div class="like-button-div">
-                              <button type="button" class="btn" @click="toggleLike(index)" @mouseenter="hovered[index] = true"
-                                      @mouseleave="hovered[index] = false" style="padding: 0">
-                                <i :class="heartClass(index)" style="color: #da2b2b;"></i>
-                              </button>
-                            </div>
-
-
+              <div class="like-button-div">
+                <button type="button" class="btn" @click="toggleLike(index)" @mouseenter="hovered[index] = true"
+                        @mouseleave="hovered[index] = false" style="padding: 0">
+                  <i :class="heartClass(index)" style="color: #da2b2b;"></i>
+                </button>
+              </div>
             </div>
           </div>
         </div>
@@ -80,14 +78,14 @@ const props = defineProps({
 
     <br>
 
-        <div class="text-center">
-          <v-pagination
-              v-model="page"
-              :length="10"
-              prev-icon="mdi-menu-left"
-              next-icon="mdi-menu-right"
-          ></v-pagination>
-        </div>
+    <div class="text-center">
+      <v-pagination
+          v-model="page"
+          :length="10"
+          prev-icon="mdi-menu-left"
+          next-icon="mdi-menu-right"
+      ></v-pagination>
+    </div>
   </div>
 </template>
 
